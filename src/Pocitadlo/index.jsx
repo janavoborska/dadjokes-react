ce36845f124c436d387384278a038db471d891e2
janavoborska <jana.dostalov@seznam.cz>
2021-05-29 13:52:45 +0200

    :hotovo

---
+++ src/Pocitadlo/index.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+export const Pocitadlo = (props) => {
+  const [pocet, setPocet] = useState(0);
+
+  return (
+    <>
+      <button
+        className={`btn-like btn-like--${props.typ}`}
+        onClick={() => setPocet(pocet + 1)}
+      ></button>
+      <span id="likes-up" className={`likes-count likes-count--${props.typ}`}>
+        {pocet}
+      </span>
+    </>
+  );
+};
